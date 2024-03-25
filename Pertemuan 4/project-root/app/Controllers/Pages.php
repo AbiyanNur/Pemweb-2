f2d91ae5892c3d63de3b41d88321ec38d163aba1
AbiyanNur <nurabiyan99@gmail.com>
2024-03-25 11:57:59 +0700

    create layout in view Lanjutan

--- Pertemuan 4/project-root/app/Controllers/Pages.php
+++ Pertemuan 4/project-root/app/Controllers/Pages.php
@@ -1,26 +1,45 @@
-<?php namespace App\Controllers;
+<?php
+
+namespace App\Controllers;
 
 class Pages extends BaseController
 {
     public function index()
     {
         $data = [
-            'title' =>'Selamat Datang | Website Anda', 
-            'tes' => ['satu', 'dua', 'tiga'] 
+            'title' => 'Selamat Datang | Website Anda',
+            'tes' => ['satu', 'dua', 'tiga']
         ];
-        echo view ('layout/header',$data);
-        echo view ('pages/home',$data);
-        echo view ('layout/footer');
-    }
-    public function about()
-{
-    $data = [
-        'title' =>'Selamat Datang | Unipdu Press',
-        'tes' => ['satu', 'dua', 'tiga'] 
-    ];
-    echo view ('layout/header',$data);
-    echo view ('pages/about',$data);
-    echo view ('layout/footer');
-}
-}
 
+        return view('pages/home', $data);
+    }
+
+    public function about()
+    {
+        $data = [
+            'title' => 'Selamat Datang | Unipdu Press',
+        ];
+        return view('pages/about', $data);
+    }
+
+    public function contact()
+    {
+        $data = [
+            'title' => 'contact',
+            'addres' =>
+            [
+                [
+                    'tipe' => 'Rumah',
+                    'alamat' => 'Mojojejer',
+                    'kota' => 'Jombang'
+                ],
+                [
+                    'tipe' => 'kantor',
+                    'alamat' => 'Mojowarno',
+                    'kota' => 'Jombang'
+                ]
+            ]
+        ];
+        return view('pages/contact', $data);
+    }
+}
--- Pertemuan 4/project-root/app/Views/Pages/about.php
+++ Pertemuan 4/project-root/app/Views/Pages/about.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?= $this->extend('layout/template'); ?>
+<?= $this->section('content'); ?>
+
 <body>
     <h1>halaman About</h1>
-    
+
 </body>
-</html>
+<?= $this->endSection(); ?>
